1d1c0aacd517f15962c9635793ed80249ed89b60
Thomas Glatt <tglatt@gmail.com>
2019-06-27 07:26:55 +0200

    test: use snapshot

--- packages/api/__test__/unit/service/alert/alert.email.spec.ts
+++ packages/api/__test__/unit/service/alert/alert.email.spec.ts
@@ -29,96 +29,15 @@ const alert: Alert = {
 it("email of alert should be equal to", () => {
   expect(
     getAlertEmail({ ...alert, alert_type: "closedWithDebutSupFin" })
-  ).toEqual({
-    to: [
-      {
-        email: "usager@wif.fr",
-        name: "usager@wif.fr"
-      }
-    ],
-    // tslint:disable-next-line: object-literal-sort-keys
-    bcc: [
-      {
-        email: "contact@workinfrance.beta.gouv.fr",
-        name: "contact@workinfrance.beta.gouv.fr"
-      }
-    ],
-    cci: [
-      {
-        email: "instructor2@direccte.gouv.fr",
-        name: "instructor2@direccte.gouv.fr"
-      },
-      {
-        email: "instructor3@direccte.gouv.fr",
-        name: "instructor3@direccte.gouv.fr"
-      }
-    ],
-    subject: "Erreur sur votre APT WorkInFrance nº502017 / UD075",
-    bodyText:
-      "Bonjour,\n\nNous avons identifié une erreur sur votre autorisation provisoire de travail n°502017. En effet, la date de fin est antérieure à la date de début de votre autorisation qui, par conséquent, n'est pas valide.\n\nSelon l'article L.242-1 du Code des relations entre le public et l'administration (https://www.legifrance.gouv.fr/affichCodeArticle.do?cidTexte=LEGITEXT000031366350&idArticle=LEGIARTI000031367657&dateTexte=&categorieLien=cid) l'administration ne peut revenir sur une décision créatrice de droits. Ainsi, les agents du service de main oeuvre étrangère de la Direccte ne peuvent revenir sur cette décision.\n\nNous vous suggérons de déposer un nouveau dossier le plus rapidement possible.\n\nBonne journée,\n\n\nL'équipe WorkInFrance\n\n\n—\n\n\nMerci de ne pas répondre à cet email"
-  });
+  ).toMatchSnapshot();
 
   expect(
     getAlertEmail({ ...alert, alert_type: "closedWithoutDateDebutOrDateFin" })
-  ).toEqual({
-    to: [
-      {
-        email: "usager@wif.fr",
-        name: "usager@wif.fr"
-      }
-    ],
-    // tslint:disable-next-line: object-literal-sort-keys
-    bcc: [
-      {
-        email: "contact@workinfrance.beta.gouv.fr",
-        name: "contact@workinfrance.beta.gouv.fr"
-      }
-    ],
-    cci: [
-      {
-        email: "instructor2@direccte.gouv.fr",
-        name: "instructor2@direccte.gouv.fr"
-      },
-      {
-        email: "instructor3@direccte.gouv.fr",
-        name: "instructor3@direccte.gouv.fr"
-      }
-    ],
-    subject: "Erreur sur votre APT WorkInFrance nº502017 / UD075",
-    bodyText:
-      "Bonjour,\n\nNous avons identifié une erreur sur votre autorisation provisoire de travail nº502017. En effet, il n'y a pas de date de début et/ou de date de fin sur votre autorisation qui, par conséquent, n'est pas valide.\n\nSelon l'article L.242-1 du Code des relations entre le public et l'administration (https://www.legifrance.gouv.fr/affichCodeArticle.do?cidTexte=LEGITEXT000031366350&idArticle=LEGIARTI000031367657&dateTexte=&categorieLien=cid) l'administration ne peut revenir sur une décision créatrice de droits. Ainsi, les agents du service de main oeuvre étrangère de la Direccte ne peuvent revenir sur cette décision.\n\nNous vous suggérons de déposer un nouveau dossier le plus rapidement possible.\n\nBonne journée,\n\n\nL'équipe WorkInFrance\n\n\n—\n\n\nMerci de ne pas répondre à cet email"
-  });
+  ).toMatchSnapshot();
 
   expect(
     getAlertEmail({ ...alert, alert_type: "closedWithSupOneYear" })
-  ).toEqual({
-    to: [
-      {
-        email: "usager@wif.fr",
-        name: "usager@wif.fr"
-      }
-    ],
-    // tslint:disable-next-line: object-literal-sort-keys
-    bcc: [
-      {
-        email: "contact@workinfrance.beta.gouv.fr",
-        name: "contact@workinfrance.beta.gouv.fr"
-      }
-    ],
-    cci: [
-      {
-        email: "instructor2@direccte.gouv.fr",
-        name: "instructor2@direccte.gouv.fr"
-      },
-      {
-        email: "instructor3@direccte.gouv.fr",
-        name: "instructor3@direccte.gouv.fr"
-      }
-    ],
-    subject: "Erreur sur votre APT WorkInFrance nº502017 / UD075",
-    bodyText:
-      "Bonjour,\n\nNous avons identifié une erreur sur votre autorisation provisoire de travail n°502017. En effet, la durée de votre autorisation est supérieure à 12 mois.\n\nSelon l'alinéa 14 de l'article R.5221-3 du Code du travail (https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000038189414&cidTexte=LEGITEXT000006072050&dateTexte=20190301) une autorisation provisoire de travail a une durée maximale de douze mois.\n\nNous vous suggérons de déposer un nouveau dossier à partir du 27/11/2019\n\nBonne journée,\n\n\nL'équipe WorkInFrance\n\n\n—\n\n\nMerci de ne pas répondre à cet email"
-  });
+  ).toMatchSnapshot();
 
   expect(
     getAlertEmail({ ...alert, alert_type: "initiatedAndDelayTooLong" })
