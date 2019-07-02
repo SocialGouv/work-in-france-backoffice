c4ffaf81a520e789477a737a13f4fab02f00fab0
Thomas Glatt <tglatt@gmail.com>
2019-07-02 14:51:03 +0200

    fix(api): make validity-check API compatible with frontend

--- packages/api/src/controller/validity-check.controller.ts
+++ packages/api/src/controller/validity-check.controller.ts
@@ -14,21 +14,23 @@ export const validityCheckController = {
       .toPromise();
     if (!data) {
       ctx.body = {
-        data: {},
+        data: [],
         status: "invalid"
       };
     } else {
       ctx.body = {
-        data: {
-          date_de_debut_apt: data.date_de_debut_apt,
-          date_de_fin_apt: data.date_de_fin_apt,
-          date_de_naissance: data.date_de_naissance,
-          ds_id: data.dossier_id,
-          has_expired: data.has_expired,
-          nom: data.nom,
-          prenom: data.prenom,
-          siret: data.siret
-        },
+        data: [
+          {
+            date_de_debut_apt: data.date_de_debut_apt,
+            date_de_fin_apt: data.date_de_fin_apt,
+            date_de_naissance: data.date_de_naissance,
+            ds_id: data.dossier_id,
+            has_expired: data.has_expired,
+            nom: data.nom,
+            prenom: data.prenom,
+            siret: data.siret
+          }
+        ],
         status: "valid"
       };
     }
