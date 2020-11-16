06f3900a148cce37caa97937b74227bea124fe53
Douglas DUTEIL <douglasduteil@gmail.com>
2020-11-16 12:13:53 +0100

    fix(docker): use @socialgouv/yarn-workspace-focus-instal

--- packages/api/src/database/config.ts
+++ packages/api/src/database/config.ts
@@ -3,14 +3,14 @@ import { Model } from "objection";
 
 export default () => {
   const knexConnection = Knex(
-    require(process.env.KNEXFILE || "../../knexfile")
+    require(process.env.KNEXFILE || "../../../knexfile")
   );
   Model.knex(knexConnection);
 };
 
 const aggregatorDatabase = Knex(
   // tslint:disable-next-line: no-var-requires
-  require(process.env.KNEXFILE_AGGREGATOR || "../../knexfile-aggregator")
+  require(process.env.KNEXFILE_AGGREGATOR || "../../../knexfile-aggregator")
 );
 
 export { aggregatorDatabase };
